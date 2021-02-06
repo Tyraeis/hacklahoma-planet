112b7c2a26a55795d55ff4439b0b63a079c89788
Gregory Maddra <bloxgate@gmail.com>
2021-02-06 16:05:09 -0600

    OO-ize Planet

--- web/index.tsx
+++ web/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/components/App';
+import {StellarObjects} from './planets/planet';
+
+let ourPlan = new StellarObjects.Planet()
+ourPlan.density = StellarObjects.EARTH_DENSITY;
+ourPlan.size = StellarObjects.EARTH_RADIUS.scale(2);
+console.log("Test Gravity: R = 2, D = 1: " + ourPlan.computePlanetaryGravityFromDensityAndRadius(ourPlan))
 
 ReactDOM.render(
     <App />,
--- web/planets/planet.tsx
+++ web/planets/planet.tsx
@@ -1,106 +1,130 @@
-import {Mass, Length, Acceleration, VolumeDensity, Temperature, Measure, kelvin, Pressure} from "safe-units";
+import {Mass, Length, Acceleration, VolumeDensity, Temperature, Measure, kelvin, Pressure, kilograms, meters, seconds, kilo, grams, centi} from "safe-units";
 
-enum HarvardSpectralType { //Star Color
-    O,
-    B,
-    A,
-    F,
-    G,
-    K,
-    M
-};
+export module StellarObjects {
 
-enum YerkesSpectralType { //Luminosity Class
-    IaP,
-    Ia,
-    Iab,
-    Ib,
-    II,
-    III,
-    IV,
-    V,
-    VI,
-    VII
-};
+    const cubic_meter = meters.squared().times(meters)
 
-type StellarClassification = {
-    color: HarvardSpectralType;
-    luminosity: YerkesSpectralType;
-};
+    export const EARTH_MASS = Measure.of(5.972e24, kilograms)
+    export const EARTH_RADIUS = Measure.of(6371, kilo(meters))
+    export const EARTH_DIAMETER = EARTH_RADIUS.scale(2)
+    export const EARTH_DENSITY = Measure.of(5.514, grams.per(centi(meters).squared().times(centi(meters))))
 
-type Star = {
-    class: StellarClassification;
-    temperature: Temperature;
-}
+    export enum HarvardSpectralType { //Star Color
+        O,
+        B,
+        A,
+        F,
+        G,
+        K,
+        M
+    };
 
-// Create a star.
-// temp is the star's temperature in Kelvin
-// luminosity is the star's Yerkes Classification
-function constructStar(temp: number, lum: YerkesSpectralType): Star {
-    let starColor: HarvardSpectralType;
-    if(temp >= 30000)
-    {
-        starColor = HarvardSpectralType.O;
-    } else if (temp >= 10000) {
-        starColor = HarvardSpectralType.B;
-    } else if (temp >= 7500) {
-        starColor = HarvardSpectralType.A;
-    } else if (temp >= 6000) {
-        starColor = HarvardSpectralType.F;
-    } else if (temp >= 5200) {
-        starColor = HarvardSpectralType.G;
-    } else if (temp >= 3700) {
-        starColor = HarvardSpectralType.K;
-    } else if (temp >= 2400) {
-        starColor = HarvardSpectralType.M;
+    export enum YerkesSpectralType { //Luminosity Class
+        IaP,
+        Ia,
+        Iab,
+        Ib,
+        II,
+        III,
+        IV,
+        V,
+        VI,
+        VII
+    };
+
+    export type StellarClassification = {
+        color: HarvardSpectralType;
+        luminosity: YerkesSpectralType;
+    };
+
+    export type Star = {
+        class: StellarClassification;
+        temperature: Temperature;
     }
 
-    let t: Temperature = Measure.of(temp, kelvin);
+    // Create a star.
+    // temp is the star's temperature in Kelvin
+    // luminosity is the star's Yerkes Classification
+    export function constructStar(temp: number, lum: YerkesSpectralType): Star {
+        let starColor: HarvardSpectralType;
+        if(temp >= 30000)
+        {
+            starColor = HarvardSpectralType.O;
+        } else if (temp >= 10000) {
+            starColor = HarvardSpectralType.B;
+        } else if (temp >= 7500) {
+            starColor = HarvardSpectralType.A;
+        } else if (temp >= 6000) {
+            starColor = HarvardSpectralType.F;
+        } else if (temp >= 5200) {
+            starColor = HarvardSpectralType.G;
+        } else if (temp >= 3700) {
+            starColor = HarvardSpectralType.K;
+        } else if (temp >= 2400) {
+            starColor = HarvardSpectralType.M;
+        }
 
-    let star = {class: {color: starColor, luminosity: lum}, temperature: t};
-    return star;
-}
+        let t: Temperature = Measure.of(temp, kelvin);
 
-enum AtmosphereType {
-    Inert, //Inert gas with no oxygen
-    Hydrogn, //Primarily hydrogen
-    Oxygen, //Mix of oxygen and inert gases
-    Methane, //Methane
-    Corrosive, //Halogens, sulfur/nitrogen oxides, etc. 
-    Reducing //Reducing agents: atomic hydrogen, boranes, cyanides, some acids, etc
-}
+        let star = {class: {color: starColor, luminosity: lum}, temperature: t};
+        return star;
+    }
 
-enum hydrosphereType {
-    None,
-    Liquid,
-    Solid
-}
+    enum AtmosphereType {
+        Inert, //Inert gas with no oxygen
+        Hydrogn, //Primarily hydrogen
+        Oxygen, //Mix of oxygen and inert gases
+        Methane, //Methane
+        Corrosive, //Halogens, sulfur/nitrogen oxides, etc. 
+        Reducing //Reducing agents: atomic hydrogen, boranes, cyanides, some acids, etc
+    }
 
-enum biosphereType {
-    None,
-    Cellular,
-    SimplePlants,
-    ComplexPlants,
-    SimpleAnimals,
-    ComplexAnimals,
-    SentientLife,
-    SapientLife
-}
+    enum hydrosphereType {
+        None,
+        Liquid,
+        Solid
+    }
 
-type Planet = {
-    size: Length; //Radius in km
-    mass: Mass; //Mass in kilograms
-    density: VolumeDensity; //Density in kilograms per cubic meter
-    gravity: Acceleration; //Acceleration due to gravity
-    parentStar: Star; //Star this planet orbits
-    atmosphere: AtmosphereType; //atmosphere composition
-    airPressure: Pressure; //pressure. We'll want to display this in millibar probably, but should store as pascals
-    hydrosphere: hydrosphereType;
-    hydrosphereElement: String;
-    averageTemperature: Temperature;
-    life: biosphereType;
-};
+    enum biosphereType {
+        None,
+        Cellular,
+        SimplePlants,
+        ComplexPlants,
+        SimpleAnimals,
+        ComplexAnimals,
+        SentientLife,
+        SapientLife
+    }
 
-function isPlanetTidallyLockedByParentStarClass(p: Planet): boolean {
-    return p.parentStar.class.color >= HarvardSpectralType.K && p.parentStar.class.luminosity >= YerkesSpectralType.V
+    export class Planet {
+        size: Length; //Radius in km
+        mass: Mass; //Mass in kilograms
+        density: VolumeDensity; //Density in kilograms per cubic meter
+        gravity: Acceleration; //Acceleration due to gravity
+        parentStar: Star; //Star this planet orbits
+        atmosphere: AtmosphereType; //atmosphere composition
+        airPressure: Pressure; //pressure. We'll want to display this in millibar probably, but should store as pascals
+        hydrosphere: hydrosphereType;
+        hydrosphereElement: String;
+        averageTemperature: Temperature;
+        life: biosphereType;
+
+        constructor() {
+
+        }
+
+        isPlanetTidallyLockedByParentStarClass(p: Planet): boolean {
+            return p.parentStar.class.color >= HarvardSpectralType.K && p.parentStar.class.luminosity >= YerkesSpectralType.V
+        }
+
+        computePlanetaryGravityFromDensityAndRadius(p: Planet): Acceleration
+        {
+            let relativeDiameter = (p.size.scale(2)).div(EARTH_DIAMETER)
+            let relativeDensity = p.density.div(EARTH_DENSITY)
+
+            let localGravity = relativeDiameter.times(relativeDensity)
+            let gravities = meters.per(seconds.squared()).scale(9.81)
+            return Measure.of(localGravity.value, gravities)
+        }
+    }   
 }
