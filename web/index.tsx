65d97f0d36a4e4a1ecf07ebc59b014fc249b33a0
Gregory Maddra <bloxgate@gmail.com>
2021-02-06 22:29:44 -0600

    Noise generation

--- web/index.tsx
+++ web/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { kelvin, Measure } from 'safe-units';
 import App from '../src/components/App';
+import { generatePlanetHeightMap } from '../src/noise-generation/noise';
 
 ReactDOM.render(
     <App />,
